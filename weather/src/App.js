b447540a89e190a3be75dd397a9ed5d46fbe8e3e
김지우 <annejwkim00@naver.com>
2025-04-04 16:22:41 +0900

    feat: 로딩 스피너 & 온도 소수점 아래 두번째 자리까지 표현

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ClipLoader from "react-spinners/ClipLoader";
 
 import { useEffect, useState } from "react";
 import WeatherBox from "./component/WeatherBox";
@@ -9,6 +10,7 @@ import WeatherButton from "./component/WeatherButton";
 function App() {
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
   const cities = ["paris", "new york", "tokyo", "seoul"];
 
   const getCurrentLocation = () => {
@@ -21,16 +23,20 @@ function App() {
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=***REMOVED***&units=metric`;
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
     setWeather(data);
+    setLoading(false);
   };
 
   const getWeatherByCity = async (city) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=***REMOVED***&units=metric`;
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
     setWeather(data);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -47,13 +53,16 @@ function App() {
 
   return (
     <div>
-      <div
-        className="weather"
-        style={{ backgroundColor: "#b7eaea", height: "100vh" }}
-      >
-        <WeatherBox weather={weather} />
-        <WeatherButton cities={cities} setCity={setCity} />
-      </div>
+      {loading ? (
+        <div className="weather">
+          <ClipLoader color="#f88c6b" loading={loading} size={150} />
+        </div>
+      ) : (
+        <div className="weather">
+          <WeatherBox weather={weather} />
+          <WeatherButton cities={cities} setCity={setCity} />
+        </div>
+      )}
     </div>
   );
 }
