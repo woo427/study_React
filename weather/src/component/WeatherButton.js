55daa5e60e95ddb6e015d980ede89096e1d5716a
김지우 <annejwkim00@naver.com>
2025-04-05 22:29:56 +0900

    feat: 버튼 클릭 시 색상, 현재 위치 버튼 활성화

--- weather/src/component/WeatherButton.js
+++ weather/src/component/WeatherButton.js
@@ -2,17 +2,43 @@ import React from "react";
 
 import { Button } from "react-bootstrap";
 
-const WeatherButton = ({ cities, setCity }) => {
+const WeatherButton = ({
+  cities,
+  setCity,
+  getCurrentLocation,
+  selectedCity,
+  setSelectedCity,
+}) => {
   return (
     <div>
-      <Button style={{ margin: 2 }} variant="dark">
+      <Button
+        style={{
+          margin: 2,
+          backgroundColor: selectedCity === "current" ? "skyblue" : "lightgray",
+          color: "black",
+          border: "none",
+        }}
+        // variant="dark"
+        onClick={() => {
+          getCurrentLocation();
+          setSelectedCity("current");
+        }}
+      >
         현재 위치
       </Button>
       {cities.map((city) => (
         <Button
-          style={{ margin: 2 }}
-          onClick={() => setCity(city)}
-          variant="dark"
+          key={city}
+          style={{
+            margin: 2,
+            backgroundColor: selectedCity === city ? "skyblue" : "lightgray",
+            border: "none",
+            color: "black",
+          }}
+          onClick={() => {
+            setCity(city);
+            setSelectedCity(city);
+          }}
         >
           {city}
         </Button>
